5e695a666b9b1163c1bdc467a717bad985535243
Hafiz Muhammad Ahmad Naeem <hafizmahmadnaeem@gmail.com>
2021-09-28 18:54:08 +0500

    Java script advance

---
+++ lec21/index.js
@@ -0,0 +1,33 @@
+console.log("\n--->>> Lec #21 || Advance js <<<---\n");
+
+// var intialArr = [1, 2, 3, 4, 5];
+// var multiplyer = [];
+// console.log("first array : ", intialArr);
+// for (index = 0; index < intialArr.length; index++) {
+//   multiplyer[index] = intialArr[index] * 2;
+// }
+// console.log("second array : ", multiplyer);
+
+// console.log("\n --->>> Arrow method <<<--- \n");
+
+// const arrowMethod = (a, b) => {
+//   return a + b;
+// };
+// console.log(arrowMethod(2, 2));
+
+// console.log("\n --->>> for each loop <<<--- \n");
+
+// var intialArr = [1, 2, 3, 4, 5];
+// intialArr.forEach((actualValue, arrayIndex) => {
+//   console.log("Actual value : ", actualValue, " index : ", arrayIndex);
+// });
+
+// console.log("\n --->>> Map loop <<<--- \n");
+// var multiplyer = [];
+// var intialArr = [1, 2, 3, 4, 5];
+// intialArr.map((actualValue, arrayIndex) => {
+//   return (
+//     actualValue * 2,
+//     console.log("Actual value : ", actualValue, " index : ", arrayIndex)
+//   );
+// });
